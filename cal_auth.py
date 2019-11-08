1c96bb4a196a031ba488bce80cacf610052f2294
encadyma <encadyma@gmail.com>
2019-11-07 23:22:52 -0800

    shooting stars

--- cal_auth.py
+++ cal_auth.py
@@ -50,7 +50,7 @@ def auth_calnet(driver, username, password):
         # Switch back to the original page
         driver.switch_to.default_content()
 
-        WebDriverWait(driver, 15).until(
+        WebDriverWait(driver, 25).until(
             ExpCond.url_changes(URL_AUTH_PORTAL)   
         )
 
@@ -77,9 +77,24 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 driver_options = Options()
-# driver_options.headless = True
+driver_options.headless = True
 
-print("It is time to authenticate into your account!")
+print(" ~~~~~ CAL DINING CHECKER ~~~~~ ")
+print(" >  mini script by @encadyma  < ")
+print(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ")
+
+
+
+print("""\n
+>> NOTICE!
+   This program is still in development.
+   Unintended behavior may result from the use of this program.
+   Further usage of this program means that you run this program at your own risk.
+
+   IMPORTANT: DO NOT TRUST YOUR PASSWORDS IN THE WRONG HANDS!
+   Always double check the source code of what you are running.
+   You may safely quit this application through CTRL+C.\n
+""")
 
 from getpass import getpass
 
@@ -95,5 +110,28 @@ print("We're authenticating you in as soon as possible...")
 if auth_calnet(driver, cal_uid, cal_pass):
     print("Loading balances from Cal Dining and Housing...")
     driver.get(URL_BALANCES_PORTAL)
+
+    btn_view_bal = "html/body/div/div[3]/div/div/div/div[1]/div/div[3]/a"
+
+    WebDriverWait(driver, 3).until(
+        ExpCond.element_to_be_clickable((By.XPATH, btn_view_bal))
+    )
+
+    driver.find_element(By.XPATH, btn_view_bal).click()
+    
+    # This is specific to a certain configuration.
+    txt_sid = "/html/body/div/div[3]/div/div/div/div[2]/div/table/tbody/tr[3]/td/b"
+    txt_flex = "/html/body/div/div[3]/div/div/div/div[2]/div/table/tbody/tr[7]/td[1]/b"
+
+    def get_txt(xpath):
+        elem = driver.find_element(By.XPATH, xpath)
+        return elem.text
+
+    print("============ STUDENT INFO ============")
+    print("Your student ID is " + get_txt(txt_sid))
+    print("Your remaining flex dollars: " + get_txt(txt_flex))
+    driver.quit()
+
 else:
     print("Quitting this application...")
+    driver.quit()

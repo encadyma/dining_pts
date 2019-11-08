6d47df9487ef534febee76e9f21973cdf7f16c9a
encadyma <encadyma@gmail.com>
2019-11-08 13:59:45 -0800

    Refactoring: moving relevant program to cli.py

--- cal_auth.py
+++ cal_auth.py
@@ -7,9 +7,43 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ExpCond
 from selenium.webdriver.common.by import By
 
-def auth_calnet(driver, username, password):
+from getpass import getpass
+
+from constants import *
+
+def auth_calnet(driver, username=None, password=None):
+    print("""
+>> [CALNET LOGIN REQUIRED BEYOND THIS POINT]
+   This program has requested to authenticate you into CalNet.
+   CalNet is the login system used by students and faculty in UC Berkeley.
+
+   If you have downloaded this program from official sources, this program 
+   will not store or deliver your CalNet login to any service other than
+   CalNet itself. Please DOUBLE CHECK the script's source code that 
+   you are running before trusting your credentials with it.
+
+   This script begins running in `./cal_auth.py`. This Python function is `cal_auth`.
+""")
+
+    raw_input("Press ENTER to proceed.")
+
+    if not username:
+        username = raw_input("CalNet username: ")
+    
+    if not password:
+        password = getpass("CalNet password (input is hidden): ")
+
     try:
         driver.get(URL_AUTH_PORTAL)
+
+        try:
+            WebDriverWait(driver, 10).until(
+                ExpCond.presence_of_element_located((By.ID, "username"))
+            )
+        except:
+            print("The authentication page timed out. Please check your internet connection.")
+            return False
+
         input_uid = driver.find_element_by_id("username")
         input_pass = driver.find_element_by_id("password")
 
@@ -31,7 +65,7 @@ def auth_calnet(driver, username, password):
             elif status_text != "":
                 print("The following authentication error occurred: " + status_text)
             else:
-                print("Login timed out. Please check your internet connection Please check your internet connection.")
+                print("Login timed out. Please check your internet connection.")
             return False
 
         # Switch to the Duo Security iFrame
@@ -45,7 +79,7 @@ def auth_calnet(driver, username, password):
 
         driver.find_element(By.XPATH, btn_2fa_xpath).click()
 
-        print("Please check your push notifications and accept it within 15 seconds.")
+        print("Please check your push notifications and accept it within 25 seconds.")
 
         # Switch back to the original page
         driver.switch_to.default_content()
@@ -60,78 +94,9 @@ def auth_calnet(driver, username, password):
         print("Something bad happened here. We got this error:", e)
         print("Here's the traceback (for advanced users):")
         
-        import traceback
+        import traceback  
         traceback.print_exc()
 
         driver.quit()
 
     return False
-
-# THIS IS TEST CODE
-# PLEASE DELETE AND MOVE TO CLI!
-
-import os
-from constants import *
-from selenium import webdriver
-
-from selenium.webdriver.firefox.options import Options
-
-driver_options = Options()
-driver_options.headless = True
-
-print(" ~~~~~ CAL DINING CHECKER ~~~~~ ")
-print(" >  mini script by @encadyma  < ")
-print(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ")
-
-
-
-print("""\n
->> NOTICE!
-   This program is still in development.
-   Unintended behavior may result from the use of this program.
-   Further usage of this program means that you run this program at your own risk.
-
-   IMPORTANT: DO NOT TRUST YOUR PASSWORDS IN THE WRONG HANDS!
-   Always double check the source code of what you are running.
-   You may safely quit this application through CTRL+C.\n
-""")
-
-from getpass import getpass
-
-print("Please authenticate into your account!")
-cal_uid = raw_input("Enter your CalNet username: ")
-cal_pass = getpass("Enter your password (this is hidden): ")
-
-driver = webdriver.Firefox(
-        executable_path=os.path.abspath("geckodriver"),
-        options=driver_options)
-print("We're authenticating you in as soon as possible...")
-
-if auth_calnet(driver, cal_uid, cal_pass):
-    print("Loading balances from Cal Dining and Housing...")
-    driver.get(URL_BALANCES_PORTAL)
-
-    btn_view_bal = "html/body/div/div[3]/div/div/div/div[1]/div/div[3]/a"
-
-    WebDriverWait(driver, 3).until(
-        ExpCond.element_to_be_clickable((By.XPATH, btn_view_bal))
-    )
-
-    driver.find_element(By.XPATH, btn_view_bal).click()
-    
-    # This is specific to a certain configuration.
-    txt_sid = "/html/body/div/div[3]/div/div/div/div[2]/div/table/tbody/tr[3]/td/b"
-    txt_flex = "/html/body/div/div[3]/div/div/div/div[2]/div/table/tbody/tr[7]/td[1]/b"
-
-    def get_txt(xpath):
-        elem = driver.find_element(By.XPATH, xpath)
-        return elem.text
-
-    print("============ STUDENT INFO ============")
-    print("Your student ID is " + get_txt(txt_sid))
-    print("Your remaining flex dollars: " + get_txt(txt_flex))
-    driver.quit()
-
-else:
-    print("Quitting this application...")
-    driver.quit()
--- cli.py
+++ cli.py
@@ -1,5 +1,64 @@
 ## The CLI interface for the Dining
 ## Pts application.
 
-def init():
-    pass
+import os
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+from constants import *
+from cal_auth import auth_calnet
+
+print("""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                 CAL DINING CHECKER                 
+              mini script by @encadyma
+                   (revision 001)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+""")
+
+print("""
+>> NOTICE!
+   This program is still in development.
+   Unintended behavior may result from the use of this program.
+   Further usage of this program means that you run this program at your own risk.
+
+   IMPORTANT: DO NOT TRUST YOUR PASSWORDS IN THE WRONG HANDS!
+   Always double check the source code of what you are running.
+   You may safely quit this application through CTRL+C.
+""")
+
+driver_options = Options()
+driver_options.headless = True
+
+driver = webdriver.Firefox(
+    executable_path=os.path.abspath("geckodriver"),
+    options=driver_options)
+
+if auth_calnet(driver):
+    print("Loading balances from Cal Dining and Housing...")
+    driver.get(URL_BALANCES_PORTAL)
+
+    btn_view_bal = "html/body/div/div[3]/div/div/div/div[1]/div/div[3]/a"
+
+    WebDriverWait(driver, 3).until(
+        ExpCond.element_to_be_clickable((By.XPATH, btn_view_bal))
+    )
+
+    driver.find_element(By.XPATH, btn_view_bal).click()
+    
+    # This is specific to a certain configuration.
+    txt_sid = "/html/body/div/div[3]/div/div/div/div[2]/div/table/tbody/tr[3]/td/b"
+    txt_flex = "/html/body/div/div[3]/div/div/div/div[2]/div/table/tbody/tr[7]/td[1]/b"
+
+    def get_txt(xpath):
+        elem = driver.find_element(By.XPATH, xpath)
+        return elem.text
+
+    print("============ STUDENT INFO ============")
+    print("Your student ID is " + get_txt(txt_sid))
+    print("Your remaining flex dollars: " + get_txt(txt_flex))
+    driver.quit()
+
+else:
+    print("Authentication failed. Quitting this application...")
+    driver.quit()
